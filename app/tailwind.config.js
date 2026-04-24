756be55dc24b84dce05e6370473e171309a799e0
Meghal09 <79741511+Meghal09@users.noreply.github.com>
2026-04-24 07:30:25 +0200

    Update tailwind.config.js

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,9 +1,9 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [],
 };
-
